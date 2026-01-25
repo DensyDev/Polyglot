eab2ffa1d91de4ec9ae76de1aa81c9391c30877c
MEFRREEX <mefrreex@gmail.com>
2026-01-26 01:29:52 +0400

    chore: update javadoc

--- api/src/main/java/org/densy/polyglot/api/formatter/TranslationFormatter.java
+++ api/src/main/java/org/densy/polyglot/api/formatter/TranslationFormatter.java
@@ -4,16 +4,16 @@ import org.densy.polyglot.api.Translation;
 import org.densy.polyglot.api.parameter.TranslationParameters;
 
 /**
- * Parameter formatter interface.
+ * Translation post formatter interface.
  */
 public interface TranslationFormatter {
 
     /**
-     * Formats text by applying parameters.
+     * Formats translation text and by applies parameters if it needs.
      *
      * @param text        the text to format
      * @param translation the translation object
-     * @param parameters  the parameters to apply
+     * @param parameters  the translation parameters
      * @return formatted text
      */
     String format(String text, Translation translation, TranslationParameters parameters);
